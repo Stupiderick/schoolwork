4d6c990d76848c6c733fcd99f3bd7618481992b3
Erick Li <ericklek1994@gmail.com>
2017-11-06 15:51:56 -0600

    Add grid option

--- tri-rasterization/rasline.py
+++ tri-rasterization/rasline.py
@@ -1,4 +1,4 @@
-def rasLine(p1x, p1y, p2x, p2y, plot=True, pointStyle='*'):
+def rasLine(p1x, p1y, p2x, p2y, plot=True, pointStyle='*', grid=False):
     '''
     find out the points that are needed to be plotted
     '''
@@ -9,14 +9,14 @@ def rasLine(p1x, p1y, p2x, p2y, plot=True, pointStyle='*'):
     if p1x == p2x:
         if p1y > p2y:
             for i in range(p2y, p1y+1):
-                coordList.append((i, p1x))
+                coordList.append((p1x, i))
             if plot:
-                plotLine(coordList, p1x-1, p2x+1, p2y, p1y, pointStyle)
+                plotLine(coordList, p1x-1, p2x+1, p2y, p1y, pointStyle, grid)
         else:
             for i in range(p1y, p2y+1):
-                coordList.append((i, p1x))
+                coordList.append((p1x, i))
             if plot:
-                plotLine(coordList, p1x-1, p2x+1, p1y, p2y, pointStyle)
+                plotLine(coordList, p1x-1, p2x+1, p1y, p2y, pointStyle, grid)
 
         return coordList
 
@@ -26,12 +26,12 @@ def rasLine(p1x, p1y, p2x, p2y, plot=True, pointStyle='*'):
             for i in range(p2x, p1x+1):
                 coordList.append((i, p1y))
             if plot:
-                plotLine(coordList, p2x, p1x, p1y-1, p2y+1, pointStyle)
+                plotLine(coordList, p2x, p1x, p1y-1, p2y+1, pointStyle, grid)
         else:
             for i in range(p1x, p2x+1):
                 coordList.append((i, p1y))
             if plot:
-                plotLine(coordList, p1x, p2x, p1y-1, p2y+1, pointStyle)
+                plotLine(coordList, p1x, p2x, p1y-1, p2y+1, pointStyle, grid)
 
         return coordList
 
@@ -42,29 +42,22 @@ def rasLine(p1x, p1y, p2x, p2y, plot=True, pointStyle='*'):
 
     slope = float((p2y - p1y) / (p2x - p1x))
     yIntercept = p1y - slope * p1x
-    print(slope)
     pointX = p1x
     pointY = p1y
 
     if slope <= 1 and slope > 0:
         while pointX != p2x or pointY != p2y:
             coordList.append((pointX, pointY))
-
             yNext = slope * (pointX + 1) + yIntercept
-
             pointX += 1
-            #print(pointX, pointY)
 
             if yNext - pointY > pointY + 1 - yNext:
                 pointY += 1
     elif slope > 1 :
         while pointX != p2x or pointY != p2y:
             coordList.append((pointX, pointY))
-
             xNext = ((pointY + 1) - yIntercept) / slope
-
             pointY += 1
-            #print(pointX, pointY)
 
             if xNext - pointX > pointX + 1 - xNext:
                 pointX += 1
@@ -73,21 +66,33 @@ def rasLine(p1x, p1y, p2x, p2y, plot=True, pointStyle='*'):
 
     if plot:
         if p2y > p1y:
-            plotLine(coordList, p1x, p2x, p1y, p2y, pointStyle)
+            plotLine(coordList, p1x, p2x, p1y, p2y, pointStyle, grid)
         else:
-            plotLine(coordList, p1x, p2x, p2y, p1y, pointStyle)
+            plotLine(coordList, p1x, p2x, p2y, p1y, pointStyle, grid)
 
     return coordList
 
 
-def plotLine(coordList, minX, maxX, minY, maxY, pointStyle):
+def plotLine(coordList, minX, maxX, minY, maxY, pointStyle, grid):
     for j in range(maxY, minY-1, -1):
-        print("{0:02d}".format(j), end='')
-        for i in range(minX, maxX+1):
-            if (i, j) in coordList:
-                print(pointStyle, end=' ')
-            else:
-                print('  ', end='')
+        print("{0:02d} ".format(j), end='')
+        # Case with grid
+        if grid:
+            for i in range(minX, maxX+1):
+                if (i, j) in coordList:
+                    print('|'+ pointStyle, end='')
+                else:
+                    print('| ', end='')
+            print('|')
+            for i in range(minX, maxX+3):
+                print('--', end='')
+        # case without grid
+        else:
+            for i in range(minX, maxX+1):
+                if (i, j) in coordList:
+                    print(pointStyle, end=' ')
+                else:
+                    print('  ', end='')
         # print a newline.
         print()
 
--- tri-rasterization/test.py
+++ tri-rasterization/test.py
@@ -1,3 +1,3 @@
 import rasline
 
-print(rasline.rasLine(0, 0, 23, 23, pointStyle='*'))
+print(rasline.rasLine(0, 23, 10, 29, pointStyle='*', grid=True))
